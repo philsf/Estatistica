1a2c4f281804427739fa93ed3111813324574f86
Felipe Figueiredo <philsf79@gmail.com>
2016-06-01 15:25:26 -0300

    9.1 15

--- Trabalhos/Estatistica-Trabalho2.R
+++ Trabalhos/Estatistica-Trabalho2.R
@@ -10,3 +10,9 @@ corr  <- function (x,y) {
   result <- (sp - n*mp)/((n - 1)*sdp)
   paste("n = ", n, "SP = ", sp, "MP = ", mp, "SDP = ", sdp, "RESULTADO = ", result)
   }
+
+## 9.1 15 ####
+x <- c(16, 25, 39, 45, 49, 64, 70, 29, 57, 22)
+y <- c(109, 122, 143, 132, 199, 185, 199, 130, 175, 118)
+corr(x,y)
+cor(x,y)
